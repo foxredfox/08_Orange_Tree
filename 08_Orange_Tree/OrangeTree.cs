ac9b4514b44cffe97438d1aa975ef359163a5817
rasmusrosenberg <rasm010h@edu.eal.dk>
2016-09-22 08:58:21 +0200

    askjdk
    kljasdlkajsd

---
+++ 08_Orange_Tree/OrangeTree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Orange_Tree
+{
+    class OrangeTree
+    {
+
+        public int Age;
+        public int Height;
+        public int TreeAlive;
+        public int NumOranges;
+        public int OrangesEaten;
+
+        public OrangeTree(int a, int b)
+        {
+
+        }
+
+        public void OneYearPasses()
+        {
+
+        }
+
+        public void EatOrange(int i)
+        {
+
+        }
+
+    }
+}
